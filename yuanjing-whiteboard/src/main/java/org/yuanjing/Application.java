4420f1ea0803626ab8b72e5ee42f799c4d9e1d5a
zhuolin <zhuolin@vortexinfo.cn>
2018-11-29 20:31:57 +0800

    白板

---
+++ yuanjing-whiteboard/src/main/java/org/yuanjing/Application.java
@@ -0,0 +1,18 @@
+package org.yuanjing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuolin
+ * @program: yuanjing
+ * @date 2018/11/29
+ * @description: ${description}
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
